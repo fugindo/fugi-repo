81ff7b0ac008fb87aa04c97a42477a7caf0a4579
Deny <denyocr.world@gmail.com>
2022-10-31 17:01:07 +0700

    .

--- generator.dart
+++ generator.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 List youtuberList = [
   {
     "name": "Erico Darmawan Handoyo",
@@ -38,4 +40,40 @@ List youtuberList = [
     "youtube": "https://www.youtube.com/channel/UC0SrBwl_lIlvR_wpYcmCG8w",
   }
 ];
-void main() {}
+
+//nodemon --exec dart generator.dart
+void main() {
+  var file = File("./README.md");
+  var content = file.readAsStringSync();
+  var lines = content.split("\n");
+
+  var newLines = [];
+  var skipLine = false;
+  for (var i = 0; i < lines.length; i++) {
+    var line = lines[i];
+    if (line.indexOf("YoutubeChannelGeneratorStart") > -1) {
+      newLines.add(line);
+      skipLine = true;
+    } else if (line.indexOf("YoutubeChannelGeneratorEnd") > -1) {
+      newLines.add(line);
+      skipLine = false;
+    } else {
+      if (skipLine == false) newLines.add(line);
+    }
+  }
+
+  var newContent = [];
+  for (var i = 0; i < youtuberList.length; i++) {
+    var item = youtuberList[i];
+    newContent.add("""
+| ${item["name"]} | [Youtube](${item["youtube"]}) . [Linkedin](https://www.linkedin.com/in/budimanrasyid/) . [GitHub](https://github.com/rsyd29) | Hacker
+"""
+        .trim());
+  }
+
+  var newUpdatedContent = newLines.join("\n");
+  newUpdatedContent = newUpdatedContent.replaceAll(
+      "<!-- YoutubeChannelGeneratorStart -->",
+      "<!-- YoutubeChannelGeneratorStart -->\n${newContent.join("\n")}");
+  file.writeAsStringSync(newUpdatedContent);
+}
