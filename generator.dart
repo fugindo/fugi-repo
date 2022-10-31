4ab94467c0b9c1cbee16685b20da84bb09634e35
Deny <denyocr.world@gmail.com>
2022-10-31 16:20:33 +0700

    .

--- generator.dart
+++ generator.dart
@@ -1,2 +1,39 @@
-List youtuberList = [];
+List youtuberList = [
+  {
+    "name": "Erico Darmawan Handoyo",
+    "youtube": "https://www.youtube.com/c/EricoDarmawanHandoyo",
+  },
+  {
+    "name": "Progammer zaman now",
+    "youtube": "https://www.youtube.com/c/ProgrammerZamanNow",
+  },
+  {
+    "name": "Abdul Aziz Ahwan",
+    "youtube": "https://www.youtube.com/c/AbdulAzizAhwanID",
+  },
+  {
+    "name": "Kuldi Project",
+    "youtube": "https://www.youtube.com/c/KuldiiProject",
+  },
+  {
+    "name": "Kopianan",
+    "youtube": "https://www.youtube.com/c/KopiAnan",
+  },
+  {
+    "name": "Capek Ngoding",
+    "youtube": "https://www.youtube.com/c/CapekNgoding",
+  },
+  {
+    "name": "Code with Bahri",
+    "youtube": "https://www.youtube.com/c/SaifulBahri27",
+  },
+  {
+    "name": "Concept Coder",
+    "youtube": "https://www.youtube.com/user/saylhendra",
+  },
+  {
+    "name": "Banua coder",
+    "youtube": "https://www.youtube.com/channel/UC0SrBwl_lIlvR_wpYcmCG8w",
+  }
+];
 void main() {}
